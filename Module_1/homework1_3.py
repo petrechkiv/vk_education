1bde389637cabc0bd095b5b28a1ea0b4d9568e0e
petrechkivov <2408275@gmail.com>
2023-11-02 10:29:45 +0300

    Добавлено решение задачи 3-1

---
+++ Module_1/homework1_3.py
@@ -0,0 +1,12 @@
+left = input()
+right = input()
+
+bool_var = True
+
+while user_num := input():
+    if left <= user_num <= right:
+        bool_var = True
+    else:
+        bool_var = False
+
+print(bool_var)
