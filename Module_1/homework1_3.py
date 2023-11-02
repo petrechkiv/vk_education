ac6aa39c776f47536b2a9b23266840272d7c3580
petrechkivov <2408275@gmail.com>
2023-11-02 14:42:11 +0300

    Исправлен код. Верный результат. Проверка данных число ли это

--- Module_1/homework1_3.py
+++ Module_1/homework1_3.py
@@ -3,10 +3,18 @@ right = input()
 
 bool_var = True
 
-while user_num := input():
-    if left <= user_num <= right:
-        bool_var = True
-    else:
-        bool_var = False
+while True:
+    user_num = input()
+    if user_num == "":
+        break
+    if user_num.isdigit():
+        user_num = int(user_num)
+        left = int(left)
+        right = int(right)
+        if left <= user_num <= right:
+            bool_var = True
+        else:
+            bool_var = False
+
 
 print(bool_var)
